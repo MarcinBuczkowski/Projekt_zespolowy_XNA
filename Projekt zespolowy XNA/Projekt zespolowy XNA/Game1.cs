0c770fef0c2f34d0d30735706e8ccb868884b257
Marcin Buczkowski <thewitcher1@gmail.com>
2012-12-22 15:36:58 +0100

    Projekt uaktualniony o kolizje.
    Uderzenie tyłem samochodu o bandę wyrzuca samochód poza trasę.

--- Projekt zespolowy XNA/Projekt zespolowy XNA/Game1.cs
+++ Projekt zespolowy XNA/Projekt zespolowy XNA/Game1.cs
@@ -11,13 +11,6 @@ using Microsoft.Xna.Framework.Media;
 
 namespace Projekt_zespolowy_XNA
 {
-
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary> 
-
-
-
     public class Game1 : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
@@ -27,10 +20,19 @@ namespace Projekt_zespolowy_XNA
         Texture2D spriteTexture;
         public Rectangle spriteRectangle;
 
+        Texture2D autoTexture;
+        Rectangle autoRectangle;
+        Rectangle MapRectangle;
+
+        Color[] spriteTextureData;
+        Color[] autoTextureData;
+        Color[] MapTexturedata;
+
         //Centralna czêœæ obrazka
         Vector2 spriteOrigin;
 
         public Vector2 spritePosition;
+        Vector2 AiCarPosition;
         float rotation;
 
         Vector2 spriteVelocity;
@@ -42,9 +44,6 @@ namespace Projekt_zespolowy_XNA
         Camera camera;
         Texture2D backgroundTexture;
         Vector2 backgroundPosition;
-
-
-
 
         public Game1()
         {
@@ -91,10 +90,22 @@ namespace Projekt_zespolowy_XNA
             //Pozycja samochodu na trasie
             spritePosition = new Vector2(300, 295);
 
-            backgroundTexture = Content.Load<Texture2D>("Background");
+            autoTexture = Content.Load<Texture2D>("auto2");
+            AiCarPosition = new Vector2(600, 295);
+            //Pozycja samochodu na trasie
+           
+
+            backgroundTexture = Content.Load<Texture2D>("trasa");
             //Po³o¿enie wyœwietlanej trasy
             backgroundPosition = new Vector2(-400, 0);
+            MapTexturedata = new Color[backgroundTexture.Width*backgroundTexture.Height];
+            backgroundTexture.GetData(MapTexturedata);
 
+            spriteTextureData = new Color[spriteTexture.Width * spriteTexture.Height];
+            spriteTexture.GetData(spriteTextureData);
+
+            autoTextureData = new Color[autoTexture.Width * autoTexture.Height];
+            autoTexture.GetData(autoTextureData);
         }
 
 
@@ -104,15 +115,13 @@ namespace Projekt_zespolowy_XNA
         /// all content.
         /// </summary>
 
-
-
-
         protected override void UnloadContent()
         {
+            autoTexture.Dispose();
+            spriteTexture.Dispose();
+            backgroundTexture.Dispose();
             // TODO: Unload any non ContentManager content here
         }
-
-
 
         /// <summary>
         /// Allows the game to run logic such as updating the world,
@@ -128,20 +137,33 @@ namespace Projekt_zespolowy_XNA
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
 
-            spriteRectangle = new Rectangle((int)spritePosition.X, (int)spritePosition.Y,
-                spriteTexture.Width, spriteTexture.Height);
-            spritePosition = spriteVelocity + spritePosition;
+            Matrix MapTransform = Matrix.CreateTranslation(new Vector3(backgroundPosition, 0.0f));
+
+            Matrix AiCarTransform =
+                Matrix.CreateTranslation(new Vector3(AiCarPosition, 0.0f));
+
+            Matrix UserCarTransform =
+                    Matrix.CreateTranslation(new Vector3(-spriteOrigin, 0.0f)) *
+                // Matrix.CreateScale(block.Scale) *  would go here
+                    Matrix.CreateRotationZ(rotation) *
+                    Matrix.CreateTranslation(new Vector3(spritePosition, 0.0f));
+
+            spriteRectangle = Collisions.CalculateBoundingRectangle(
+                         new Rectangle(0, 0, spriteTexture.Width, spriteTexture.Height),
+                         UserCarTransform);
+
+            autoRectangle = new Rectangle((int)AiCarPosition.X, (int)AiCarPosition.Y, autoTexture.Width, autoTexture.Height);
+
+            MapRectangle = new Rectangle((int)backgroundPosition.X, (int)backgroundPosition.Y, backgroundTexture.Width, backgroundTexture.Height);
+
             spriteOrigin = new Vector2(spriteRectangle.Width / 2, spriteRectangle.Height / 2);
-
-
 
             //Poruszanie do przodu uwzglêdniaj¹c rotacjê
             if (Keyboard.GetState().IsKeyDown(Keys.Up))
             {
                 //Obroty samochodu
-                if (Keyboard.GetState().IsKeyDown(Keys.Right)) rotation += 0.1f;
-                if (Keyboard.GetState().IsKeyDown(Keys.Left)) rotation -= 0.1f;
-
+                if (Keyboard.GetState().IsKeyDown(Keys.Right)) rotation += 0.05f;
+                if (Keyboard.GetState().IsKeyDown(Keys.Left)) rotation -= 0.05f;
                 spriteVelocity.X = (float)Math.Cos(rotation) * tangentialVelocity;
                 spriteVelocity.Y = (float)Math.Sin(rotation) * tangentialVelocity;
             }
@@ -151,22 +173,26 @@ namespace Projekt_zespolowy_XNA
                 float j = spriteVelocity.Y;
 
                 if (spriteVelocity != Vector2.Zero) spriteVelocity *= 1 - friction;
-
+            }
+            if (autoRectangle.Intersects(spriteRectangle))
+            {
+                if (Collisions.IntersectPixels(AiCarTransform, autoTexture.Width, autoTexture.Height, autoTextureData, UserCarTransform, spriteTexture.Width, spriteTexture.Height, spriteTextureData))
+                {
+                    spriteVelocity.X = -spriteVelocity.X;
+                    spriteVelocity.Y = -spriteVelocity.Y;
+                }
+            }
+           
+            if (Collisions.Intersect(UserCarTransform, spriteTexture.Width, spriteTexture.Height, spriteTextureData, MapTransform, backgroundTexture.Width, backgroundTexture.Height, MapTexturedata))
+            {
+                spriteVelocity.X = -spriteVelocity.X;
+                spriteVelocity.Y = -spriteVelocity.Y;
             }
 
-
+            spritePosition = (spriteVelocity + spritePosition);
             camera.Update(gameTime, this);
             base.Update(gameTime);
         }
-
-
-
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-
-
 
         protected override void Draw(GameTime gameTime)
         {
@@ -178,7 +204,7 @@ namespace Projekt_zespolowy_XNA
                 camera.transform);
 
             spriteBatch.Draw(backgroundTexture, backgroundPosition, Color.White);
-
+            spriteBatch.Draw(autoTexture, autoRectangle, Color.White);
             //Rysowanie tekstury i ustawianie koloru na transparentny, rotacji, centralna czêœæ obrazka, bez efektów
             spriteBatch.Draw(spriteTexture, spritePosition, null, Color.White, rotation, spriteOrigin, 1f, SpriteEffects.None, 0);
             spriteBatch.End();
